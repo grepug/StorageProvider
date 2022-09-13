4d171054cf28ce4335629eeca03e73e709a9d448
Kai Shao <grepug@icloud.com>
2022-09-13 16:07:05 +0800

    filter out dbChange events when iCloud is syncing

--- Sources/StorageProvider/StorageProvider+Utils.swift
+++ Sources/StorageProvider/StorageProvider+Utils.swift
@@ -7,12 +7,26 @@
 
 import Combine
 import Foundation
+import CloudKitSyncMonitor
 
 public extension StorageProvider {
+    static var shouldUpdateDuring_iCloudSyncing: Bool {
+        guard let monitor = SyncMonitor.shared as? SyncMonitorWithCloudKit else {
+            return true
+        }
+           
+        guard monitor.isCloudEnabled else {
+            return true
+        }
+        
+        return SyncMonitor.shared.syncStateSummary == .succeeded
+    }
+    
     static var dbChangePublisher: AnyPublisher<Void, Never> {
          NotificationCenter.default
              .publisher(for: .NSManagedObjectContextObjectsDidChange)
              .merge(with: NotificationCenter.default.publisher(for: .NSManagedObjectContextDidSave))
+             .filter { _ in shouldUpdateDuring_iCloudSyncing }
              .debounce(for: 0.3, scheduler: RunLoop.current)
              .map { _ in }
              .eraseToAnyPublisher()
